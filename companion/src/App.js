d7e8e6ddc82dee34f2d1725956b10937f13e4237
Ain't No Way <tinaojanashley@gmail.com>
2024-11-03 14:14:54 +0800

    react.js files
    added all necessary files for react

---
+++ companion/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>companiON</h1>
+      </header>
+      <main className="App-content">
+        <p>Senior Care Services Web landing page!</p>
+      </main>
+      <footer className="App-footer">
+        <p>&copy; Web Landing Page</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
