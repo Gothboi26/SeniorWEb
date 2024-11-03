4a7070522c5d06836fcad846775697cd3a1a8971
Ain't No Way <tinaojanashley@gmail.com>
2024-11-03 18:52:29 +0800

    Navbar
    added navigation bar

--- companion/src/App.js
+++ companion/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
+import Navbar from "./Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <h1>companiON</h1>
       </header>
       <main className="App-content">
-        <p>Senior Care Services Web landing page!</p>
+        <p>Senior care services web landing page!</p>
       </main>
       <footer className="App-footer">
-        <p>&copy; Web Landing Page</p>
+        <p>&copy; 2024</p>
       </footer>
     </div>
   );
