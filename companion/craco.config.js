0cd5c3c7e3da3fd99f84004032fbe3fa7517e93d
GIGA_ASH <tinaojanashley@gmail.com>
2025-03-17 19:24:36 -0700

    package
    revert if fail

---
+++ companion/craco.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    webpack: {
+      configure: (webpackConfig) => {
+        // Find and remove CSS Minimizer Plugin
+        webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.filter(
+          (plugin) => plugin.constructor.name !== "CssMinimizerPlugin"
+        );
+        return webpackConfig;
+      },
+    },
+  };
+  
