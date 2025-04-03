5f95b4a0d8d4979787ae94ed501394dc58ecce0b
mtruvo <115444201+mtruvo@users.noreply.github.com>
2025-04-03 20:43:12 +0800

    delete backend

--- Appointment(backend)/appointment.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE appointments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    service VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
-    status ENUM('Pending', 'Approved', 'Rejected') NOT NULL DEFAULT 'Pending',
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
