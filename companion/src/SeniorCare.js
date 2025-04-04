2cda9ab984bfde1a5f15f84c6d749e91c487c84d
GIGA_ASH <tinaojanashley@gmail.com>
2025-04-03 19:49:00 -0700

    Revert "revert if fail"
    This reverts commit 7689496ee48877a00f1cefc53042bc9ba1630c20.

--- companion/src/SeniorCare.js
+++ companion/src/SeniorCare.js
@@ -43,7 +43,7 @@ const SeniorCare = ({ role, handleLogout }) => {
   useEffect(() => {
     const fetchAppointments = async () => {
       try {
-        const response = await fetch("https://companionbackend-production.up.railway.app/appointments.php", {
+        const response = await fetch("companionbackend-production.up.railway.app/appointments.php", {
           method: "GET",
           credentials: "include",
         });
@@ -118,7 +118,7 @@ const SeniorCare = ({ role, handleLogout }) => {
     };
 
     try {
-      const response = await fetch("https://companionbackend-production.up.railway.app/appointments.php", {
+      const response = await fetch("companionbackend-production.up.railway.app/appointments.php", {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
