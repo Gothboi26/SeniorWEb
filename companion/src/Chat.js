7ead4b32339b9d845528909264d73f668f8fade1
GIGA_ASH <tinaojanashley@gmail.com>
2025-04-27 14:13:09 -0700

    Revert "revert if fail"
    This reverts commit 649806cd1278083c503bd82ee62b1ba794167c20.

--- companion/src/Chat.js
+++ companion/src/Chat.js
@@ -30,7 +30,7 @@ function Chat({ role, handleLogout }) {
 
   // Step 1: Fetch the username from the PHP session
   useEffect(() => {
-    fetch("https://backend-production-4629.up.railway.app/get_username.php", {
+    fetch("https://websocket-production-b0d9.up.railway.app/get_username.php", {
       method: "GET",
       credentials: "include",
     })
@@ -49,7 +49,7 @@ function Chat({ role, handleLogout }) {
   useEffect(() => {
     if (!username) return;
 
-    const socket = new WebSocket("ws:https://websocket-production-b0d9.up.railway.app/");
+    const socket = new WebSocket("ws://localhost:8080");
 
     socket.onopen = () => {
       socket.send(
